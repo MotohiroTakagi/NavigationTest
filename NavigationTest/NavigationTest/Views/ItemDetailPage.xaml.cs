649fdef5160fa027bf40aa2cbf9a5b2e3c853946
MotohitoTakagi <takagi.motohiro@japanmedicalcompany.co.jp>
2022-10-28 16:45:21 +0900

    first commit

---
+++ NavigationTest/NavigationTest/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NavigationTest.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NavigationTest.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
